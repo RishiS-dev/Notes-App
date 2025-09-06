dafd3fa6be2c36dee214b845e338f574e79e31e9
Rishi Sundaresan <rishi200322@gmail.com>
2025-09-06 13:37:44 +0530

    implemented rate limiter using redis
    Implemented the rate limiter using Redis
    Done with backend routes, controllers and middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,26 @@ import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "../config/db.js"
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config()
 
 const app = express()
 
-connectDB()
-
 //middleware
 app.use(express.json())
+app.use(rateLimiter)
+
+//simple custon middleware
+// app.use((req,res,next)=>{
+//     console.log(`Req method is ${req.method} & Req url is ${req.url}`)
+//     next()
+// })
 
 app.use("/api/notes",notesRoutes)
 
-app.listen(process.env.PORT, () =>{
+connectDB().then(()=>{
+    app.listen(process.env.PORT, () =>{
     console.log("Server started on ",process.env.PORT)
+})
 })
