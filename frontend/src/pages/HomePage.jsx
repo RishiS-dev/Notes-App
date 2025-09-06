2974c4a3ab799d30a25775365e2cf448da94dcab
Rishi Sundaresan <rishi200322@gmail.com>
2025-09-06 16:42:32 +0530

    Front end
    Started frontend with React, Tailwind css, Daisy UI

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/navbar'
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen">
+        <NavBar />
+    </div>
+  )
+}
+
+export default HomePage
